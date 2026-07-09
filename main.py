13e14b479e22c864500e18b7b94fa8ede10cea9d
TooSpook <barnabasbozso05@gmail.com>
2026-07-09 19:54:32 +0200

    add select button

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import ctypes
 from main_window import MainWindow
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication
 from PyQt6.QtGui import QIcon
 
-ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("csvcleaner.alpha.1.5")
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("csvcleaner.pre-alpha.1.5")
 
 def main():
     app = QApplication([]) 
--- main_window.py
+++ main_window.py
@@ -1,11 +1,20 @@
-from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog
+from PyQt6.QtCore import QSize
+from PyQt6.QtWidgets import QMainWindow, QPushButton
 from PyQt6.QtGui import QIcon
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        button = QPushButton("SELECT")
+        button.setMinimumSize(120, 40)
+        button.setMaximumSize(200, 50)
+        button.setStyleSheet("padding: 12px 24px; font-size: 14px;")
+
         self.setWindowTitle("CSV cleaner")
-        self.setFixedSize(QSize(512, 384))
+        self.setMinimumSize(QSize(512, 384))
         self.setWindowIcon(QIcon("resources/app_icon.png"))
+        self.setCentralWidget(button)
+
+    def file_selection(self):
+        pass
