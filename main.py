78d4972ad200f061e2716315dcefec229d97f1a0
TooSpook <barnabasbozso05@gmail.com>
2026-07-09 20:19:21 +0200

    feat: add file selection

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from main_window import MainWindow
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtGui import QIcon
 
-ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("csvcleaner.pre-alpha.1.5")
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("csvcleaner.pre-alpha.1.7")
 
 def main():
     app = QApplication([]) 
--- main_window.py
+++ main_window.py
@@ -1,5 +1,5 @@
 from PyQt6.QtCore import QSize
-from PyQt6.QtWidgets import QMainWindow, QPushButton
+from PyQt6.QtWidgets import QMainWindow, QPushButton, QFileDialog
 from PyQt6.QtGui import QIcon
 
 class MainWindow(QMainWindow):
@@ -10,11 +10,16 @@ class MainWindow(QMainWindow):
         button.setMinimumSize(120, 40)
         button.setMaximumSize(200, 50)
         button.setStyleSheet("padding: 12px 24px; font-size: 14px;")
+        button.clicked.connect(self.file_selection)
 
         self.setWindowTitle("CSV cleaner")
         self.setMinimumSize(QSize(512, 384))
         self.setWindowIcon(QIcon("resources/app_icon.png"))
         self.setCentralWidget(button)
 
-    def file_selection(self):
-        pass
+    def file_selection(self) -> str:
+        path, _ = QFileDialog.getOpenFileName(self, "Open CSV file", "", "CSV Files (*.csv);;All Files (*)")
+        if not path:
+            return
+        else:
+            return path
